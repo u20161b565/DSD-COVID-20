1e5f8b14cb7f9a0841ed82fbcaba2239decf4283
Claudio Temple <u20161b565@upc.edu.pe>
2020-04-19 21:56:10 -0500

    Proyecto Inicial

---
+++ src/main/java/pe/upc/covid20/reporteciudadano/ReporteCiudadanoApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.covid20.reporteciudadano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteCiudadanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReporteCiudadanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/upc/covid20/reporteciudadano/ReporteCiudadanoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.covid20.reporteciudadano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteCiudadanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
