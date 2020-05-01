4799ac797be877e6a241c1c3c1b6a7cdf800a15b
andresinope77 <63989998+andresinope77@users.noreply.github.com>
2020-04-30 20:36:44 -0500

    Actualizaciones de Clases
    Incorporación de código para creación de columnas en base de datos.

--- src/main/java/pe/upc/covid20/reporteciudadano/entidades/Reporte.java
+++ src/main/java/pe/upc/covid20/reporteciudadano/entidades/Reporte.java
@@ -7,26 +7,85 @@ import java.io.Serializable;
 @Entity
 public class Reporte implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TipoReporte_id", referencedColumnName = "id")
     private TipoReporte tipoReporte;
 
-    private int tipo;
     private String direccion;
+    private int latitud;
+    private int longitud;
+    private String status;
     @Column(length = 5000)
-    private String comentario;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "departamento_id", referencedColumnName = "id")
-    private Departamento departamento;
+    private String descripcion;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "distrito_id", referencedColumnName = "id")
     private Distrito distrito;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "provincia_id", referencedColumnName = "id")
-    private Provincia provincia;
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public TipoReporte getTipoReporte() {
+        return tipoReporte;
+    }
+
+    public void setTipoReporte(TipoReporte tipoReporte) {
+        this.tipoReporte = tipoReporte;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(int latitud) {
+        this.latitud = latitud;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Distrito getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(Distrito distrito) {
+        this.distrito = distrito;
+    }
 }
