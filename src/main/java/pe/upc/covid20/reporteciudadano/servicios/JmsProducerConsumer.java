222860f2c2f0910d46f5a7945b913ce9b660a042
andresinope77 <63989998+andresinope77@users.noreply.github.com>
2020-05-02 00:14:00 -0500

    Creación de Cola

---
+++ src/main/java/pe/upc/covid20/reporteciudadano/servicios/JmsProducerConsumer.java
@@ -0,0 +1,37 @@
+package pe.upc.covid20.reporteciudadano.servicios;
+
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JmsProducerConsumer {
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Value("${jms.cola.envio}")
+    String destinationQueue;
+
+    @Value("${jms.cola.respuesta}")
+    String responseQueue;
+
+    public void enviarRecibir(String msg) {
+        String id = UUID.randomUUID().toString();//
+        jmsTemplate.convertAndSend(destinationQueue, msg, m -> {
+            m.setJMSCorrelationID(id);
+            return m;
+        });
+        System.out.println("RIPLEY: Enviando " + msg);
+        System.out.println("RIPLEY: Enviando con CorrId: " + id);
+        //espera respuesta solo de un mensaje cuyo corrId es el mismo del que envió
+        //jmsTemplate.setReceiveTimeout(1000);
+        String responseMessage = (String) jmsTemplate
+                .receiveSelectedAndConvert(responseQueue,
+                        "JMSCorrelationID='" + id + "'");
+        System.out.println("RIPLEY RESPUESTA:" + responseMessage);
+        System.out.println("RIPLEY RESPUESTA CORR ID:" + id);
+    }
+
+}
